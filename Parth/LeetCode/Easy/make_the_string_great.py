ed26f66dfc7212baffe312ded9ae0e86c001bd82
shahp7575 <shahp7575@gmail.com>
2020-08-24 10:13:04 -0400

    incomplete soln

--- Parth/LeetCode/Easy/make_the_string_great.py
+++ Parth/LeetCode/Easy/make_the_string_great.py
@@ -11,18 +11,22 @@ class Solution:
 
     def makeGood(self, s: str) -> str:
 
-        upper = [char for char in s if char.isupper()]
-        print(upper)
-        new_str = ""
-        while len(upper) > 0:
-            idx = s.find(upper[0])
-            new_str = s[]
-            upper.pop()
+        s_list = [char for char in s]
+        print(s_list)
+        new_str = []
+        
+        for i in s_list:
             
+            if i.isupper():
+                s_list.remove(i)
+                s_list.pop()
+
+
+        return "".join(s_list)
 
 if __name__ == "__main__":
 
     result = Solution()
-    string = 'leEeetcode'
+    string = 'abBacC'
     print(result.makeGood(string))
         
