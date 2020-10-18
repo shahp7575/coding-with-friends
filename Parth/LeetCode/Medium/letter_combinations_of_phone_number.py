755229c1271e3d5b05bfa8fdfdab5639e7fa489e
shahp7575 <shahp7575@gmail.com>
2020-10-18 11:52:08 -0400

    letter comb completed

--- Parth/LeetCode/Medium/letter_combinations_of_phone_number.py
+++ Parth/LeetCode/Medium/letter_combinations_of_phone_number.py
@@ -1,42 +1,49 @@
+"""
+Runtime: 32 ms
+Memory: 14 MB
+"""
+
 from typing import List
 
 class Solution:
 
     """
+    Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
 
+    A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
     """
 
     def letterCombinations(self, digits: str) -> List[str]:
         
+        if not digits:
+            return []
         res = []
         self.dig_dict = {
-            '2': ["a", "b", "c"],
-            '3': ["d", "e", "f"],
-            '4': ["g", "h", "i"],
-            '5': ["j", "k", "l"],
-            '6': ["m", "n", "o"],
-            '7': ["p", "q", "r", "s"],
-            '8': ["t", "u", "v"],
-            '9': ["w", "x", "y", "z"]
+            '2': "abc",
+            '3': "def",
+            '4': "ghi",
+            '5': "jkl",
+            '6': "mno",
+            '7': "pqrs",
+            '8': "tuv",
+            '9': "wxyz"
         }
-        self.dig_list = [self.dig_dict[d] for d in digits]
-        self.dfs(digits, [], res)
+        print(self.dig_dict[digits[0]])
+        self.dfs(digits, "", res)
         return res
 
     def dfs(self, digits, path, res):
 
-        print("digi", digits)
-
-        if path not in res and len(path) == 2:
+        if not digits:
             res.append(path)
+            return
 
-        for i in range(len(self.dig_list)):
+        for i in self.dig_dict[digits[0]]:
 
-            self.dfs(self.dig_list[i+1:], self.dig_list[i][i], res)
-
+            self.dfs(digits[1:], path + i, res)
     
 if __name__ == "__main__":
 
     result = Solution()
-    digits = "23"
+    digits = ""
     print(result.letterCombinations(digits))
